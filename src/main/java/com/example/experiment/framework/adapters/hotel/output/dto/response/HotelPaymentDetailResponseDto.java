0c413aa2f95b457e993a7a1b91b51c36747e5ff8
Norin <norinke@skybooking.info>
2023-01-27 15:46:00 +0700

    minor code refactor

--- src/main/java/com/example/experiment/framework/adapters/hotel/output/dto/response/HotelPaymentDetailResponseDto.java
+++ src/main/java/com/example/experiment/framework/adapters/hotel/output/dto/response/HotelPaymentDetailResponseDto.java
@@ -3,14 +3,12 @@ package com.example.experiment.framework.adapters.hotel.output.dto.response;
 import com.example.experiment.domain.entity.PaymentDetail;
 import com.example.experiment.domain.entity.Product;
 import com.example.experiment.domain.vo.Id;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
 
 
-@EqualsAndHashCode
 @ToString
 @Getter
 public class HotelPaymentDetailResponseDto extends PaymentDetail {
